48f2d6fe1520642daa57b81845ea64a2e91257a0
Ernesto Lopez <ernestolopez2018@fau.edu>
2021-11-03 20:03:22 -0400

    my 10th commit

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 
 
-app.use(express.static(path.join(__dirname, '/distantly-near/build')));
+app.use(express.static(path.join(__dirname, './distantly-near/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/distantly-near/build', 'index.html'));
+  res.sendFile(path.join(__dirname, './distantly-near/build', 'index.html'));
 });
 
 
